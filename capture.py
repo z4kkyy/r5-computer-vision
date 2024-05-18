29b4f9a1031b2b9b03fdeb194c6c27727c8ea198
z4kkyy <mathtaka1528@gmail.com>
2024-05-18 19:41:02 +0900

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,6 +1,6 @@
-import win32api
 import dxcam
 import numpy as np
+import win32api
 
 
 # using dxcam (https://github.com/AI-M-BOT/DXcam)
@@ -15,25 +15,31 @@ class Capture:
 
         # the portion of the screen to capture, 1/2 for 1080p
         # must be integer
-        crop_height = 540
+        crop_height = 540    # does not work if 640
         crop_width = 540 * screen_width // screen_height
 
         # the offset of the window
         # must be integer
-        x_offset = (screen_width - crop_width) // 2
-        y_offset = (screen_height - crop_height) // 2
+        self.x_offset = (screen_width - crop_width) // 2
+        self.y_offset = (screen_height - crop_height) // 2
 
         self.region = (
-            x_offset,
-            y_offset,
-            x_offset + crop_width,
-            y_offset + crop_height
+            self.x_offset,
+            self.y_offset,
+            self.x_offset + crop_width,
+            self.y_offset + crop_height
         )
         self.camera = dxcam.create(region=self.region)
 
     def capture(self) -> np.ndarray:
         """
-        Capture the screen.
+        Capture the screen. Note that dxcam.DXCamera.grab() will return None if there is no new frame since the last time called.
+
+        NOTE: To view the image:
+        from PIL import Image
+        Image.fromarray(frame).show()
+
+        :return: captured image
         """
         while True:
             image = self.camera.grab()
