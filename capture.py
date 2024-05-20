035fd1637672a60fc15c5566d2c7a53ba402792c
z4kkyy <mathtaka1528@gmail.com>
2024-05-20 23:30:32 +0900

    Change capturing method

--- capture.py
+++ capture.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 import dxcam
 import numpy as np
 import win32api
@@ -10,10 +13,15 @@ class Capture:
         Initialize the capture object.
         The region is set to the center of the screen.
         """
+        self.counter = 0
+        self.screen_capture_time = 0
+        self.logger = logging.getLogger("r5CV")
+
         screen_width = win32api.GetSystemMetrics(0)
         screen_height = win32api.GetSystemMetrics(1)
 
         # the portion of the screen to capture, 640 x 640 square in the center of the screen
+        # NOTE: captured area must be a square, so crop_height == crop_width
         crop_height = 640
         crop_width = 640
 
@@ -30,20 +38,31 @@ class Capture:
         )
         self.camera = dxcam.create(region=self.region)
 
+        self.camera.start(target_fps=144)
+
     def capture(self) -> np.ndarray:
         """
         Capture the screen. Note that dxcam.DXCamera.grab() will return None if there is no new frame since the last time called.
 
-        NOTE: To view the image:
-        from PIL import Image
-        Image.fromarray(frame).show()
-
         :return: captured image
         """
-        while True:
-            image = self.camera.grab()
-            if image is not None:
-                return image
+        # NOTE: to view the image:
+        # from PIL import Image
+        # Image.fromarray(frame).show()
+        self.counter += 1
+        start_time = time.time()
+        # while True:
+        #     image = self.camera.grab()
+        #     if image is not None:
+        #         break
+        image = self.camera.get_latest_frame()
+
+        self.screen_capture_time += time.time() - start_time
+        if self.counter % 1000 == 0:
+            print("------------------------------------------------------------------------------")
+            self.logger.info(f"Screen capture: {self.screen_capture_time:.2f} ms")
+            self.screen_capture_time = 0
+        return image
 
 
 if __name__ == "__main__":
