c24ec1e348f47f741e127a5681c17ae825ef7961
z4kkyy <mathtaka1528@gmail.com>
2025-07-07 20:12:09 +0900

    Add shutdown handling with signal handlers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import logging.config
 import os
 import platform
+import signal
 import sys
 import threading
 from time import time
@@ -98,26 +99,39 @@ if __name__ == "__main__":
     elapsed_time = 0
     readstate_time = 0
 
-    while True:
-        # This is the main loop, where the inference is executed.
-        # The inference process is executed in every iteration of the loop.
-        # Performance:
-        # - Capture: avg. 0.67 ms
-        # - Inference: avg. 7.23 ms (using best_8s.engine on TensorRT)
-        # - Iteration FPS: 72.97
-        exec_count += 1
+    def signal_handler(signum, frame):
+        logger.info("Ctrl+C detected. Shutting down gracefully...👋")
+        sys.exit(0)
 
-        # read key and mouse states
-        key_state = input_listener.get_key_state()      # hold_state, toggle_state_1, toggle_state_2, shutdown
-        mouse_state = input_listener.get_mouse_state()  # mouse_left_state, mouse_right_state
-        if key_state[3]:  # shutdown = True
-            logger.info("Shutting down...👋")
-            sys.exit()
-        time_start = time()
-        r5CV.execute(key_state, mouse_state)
-        elapsed_time += time() - time_start
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    try:
+        while True:
+            # This is the main loop, where the inference is executed.
+            # The inference process is executed in every iteration of the loop.
+            # Performance:
+            # - Capture: avg. 0.67 ms
+            # - Inference: avg. 7.23 ms (using best_8s.engine on TensorRT)
+            # - Iteration FPS: 72.97
+            exec_count += 1
 
-        if exec_count % 1000 == 0:
-            logger.info(f"Avg. iteration FPS: {1000 / elapsed_time:.2f} ({elapsed_time :.2f} ms per frame)")
+            # read key and mouse states
+            key_state = input_listener.get_key_state()      # hold_state, toggle_state_1, toggle_state_2, shutdown
+            mouse_state = input_listener.get_mouse_state()  # mouse_left_state, mouse_right_state
+            if key_state[3]:  # shutdown = True
+                logger.info("Shutting down...👋")
+                sys.exit()
+            time_start = time()
+            r5CV.execute(key_state, mouse_state)
+            elapsed_time += time() - time_start
 
-            elapsed_time = 0
+            if exec_count % 1000 == 0:
+                logger.info(f"Avg. iteration FPS: {1000 / elapsed_time:.2f} ({elapsed_time :.2f} ms per frame)")
+
+                elapsed_time = 0
+    except KeyboardInterrupt:
+        logger.info("Ctrl+C detected. Shutting down gracefully...👋")
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+    finally:
+        sys.exit(0)
